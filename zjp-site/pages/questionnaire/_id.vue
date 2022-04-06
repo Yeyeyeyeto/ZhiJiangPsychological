86bf224aca49f6895491193a2f322d74ecea7d08
yeyeyeyeto <2875381616@qq.com>
2022-04-06 16:53:47 +0800

    20220406 QuestionnaireWhether Submit Finish

--- zjp-site/pages/questionnaire/_id.vue
+++ zjp-site/pages/questionnaire/_id.vue
@@ -14,11 +14,22 @@
             <span :prop="'questionItem' + index + 'questionNum'">{{ item.questionNum }}.</span> 
             {{ item.questionTitle }}
             <br><br>
-            <input type="radio" :name="'questionItemRadio' + index" :value="1" :id="'questionRadio' + index + 't'"/><label :for="'questionRadio' + index + 't'">是</label>
-            <input type="radio" :name="'questionItemRadio' + index" :value="0" :id="'questionRadio' + index + 'f'"/><label :for="'questionRadio' + index + 'f'">否</label>            
+            <input type="radio" 
+                  :name="'questionItemRadio' + index" 
+                  :id="'questionRadio' + index + 't'"
+                  @click="radioItemChange(index, 1)"
+                  />
+                  <label :for="'questionRadio' + index + 't'">是</label>
+
+            <input type="radio" 
+                  :name="'questionItemRadio' + index" 
+                  :id="'questionRadio' + index + 'f'"
+                  @click="radioItemChange(index, 0)"
+                  />
+                  <label :for="'questionRadio' + index + 'f'">否</label>            
           </li>
         </ul>
-        <button class="questionnaire-submit-btn" @click="submit()">完成</button>
+        <button class="questionnaire-submit-btn" @click="submit(questionnaireItem.id)">完成</button>
       </div>      
 
       <div class="questionnaire-item-info">
@@ -34,7 +45,7 @@
 import '~/assets/css/questionnaire.css'
 
 export default {
-    async asyncData({ $axios, params }) {
+  async asyncData({ $axios, params }) {
       let questionnaireId = params.id;
       let response = await $axios.$get('/admin/core/questionnaire/get/' + questionnaireId);
       let question = await $axios.$get('/admin/core/questionnaire/showDetails/' + questionnaireId);
@@ -47,23 +58,36 @@ export default {
 
   data() {
     return {
-      score: [{
-
-      }]
+      score: [
+        {vaule: '1'}
+      ],
+      radioList: [
+      ]
     }
   },
 
   //此时方法在客户端的浏览器中执行，可以获取到cookie
   mounted() {
+  
   },
 
 
   methods: {
-    submit() {
-      // this.$axios.$post().then((response) => {
-        
-      // })
-      console.log(score);
+    radioItemChange(indexIn, value) {
+      this.radioList.push({
+        index: indexIn+1,
+        value: value
+      })
+    },
+
+    submit(id) {
+      console.log(this.radioList);
+      console.log(id);
+      this.$axios.$post('/api/core/questionnaireWhether/submit', {key1: id, key2: this.radioList}).then((response) => {
+        console.log(response)
+        // cookie.set('user', response.data.user)
+        // window.location.href = '/user'
+      })
     },
     
   }  
--- zjp/service-core/src/main/java/com/eternal/zjp/core/controller/api/QuestionnaireWhetherController.java
+++ zjp/service-core/src/main/java/com/eternal/zjp/core/controller/api/QuestionnaireWhetherController.java
@@ -2,7 +2,6 @@ package com.eternal.zjp.core.controller.api;
 
 
 import com.eternal.common.result.R;
-import com.eternal.zjp.core.pojo.vo.QuestionnaireWhetherVO;
 import com.eternal.zjp.core.service.QuestionnaireWhetherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * <p>
@@ -27,16 +28,31 @@ import javax.annotation.Resource;
 @RequestMapping("/api/core/questionnaireWhether")
 @Slf4j
 public class QuestionnaireWhetherController {
+
     @Resource
     private QuestionnaireWhetherService questionnaireWhetherService;
 
     @ApiOperation("问卷提交")
     @PostMapping("/submit")
-    public R submit(@RequestBody QuestionnaireWhetherVO questionnaireWhetherVO) {
+    public R submit(@RequestBody Map<String, Object> maps) {
+
+        Integer questionnaireId = (Integer) maps.get("key1");
+
+        System.out.println(questionnaireId);
+
+        ArrayList<Map<String, Integer>> radioList = (ArrayList<Map<String, Integer>>) maps.get("key2");
+
+        for (Map<String, Integer> map : radioList) {
+            int index = 0, value = 0;
+            index = map.get("index");
+            value = map.get("value");
+
+            System.out.println(index + "  " + value);
+
+        }
 
         return R.ok().message("提交成功");
     }
 
 
 }
-
